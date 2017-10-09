c2a8f18316c7259b2ea50410c3d1d23506d3a8b7
SuperLazyDog <jyoitatsu@foxmail.com>
2017-10-09 15:49:51 +0900

    finish 7th

--- First-Learning/section7_basic.js
+++ First-Learning/section7_basic.js
@@ -8,11 +8,62 @@
 //-------------------------------------------
 // 7.2 条件
 //-------------------------------------------
+print("//-------------------------------------------");
+print("// 7.2 条件");
+print("//-------------------------------------------");
+let if_date = {
+  people: {
+    a: {name: "a", age: 18},
+    b: {name: "b", age: 20},
+    c: {name: "c", age: 22}
+  },
 
+  animal: {
+    bird: {power: 100, def: 80}
+  }
+}
+
+if (if_date.people.a.name !== null) {
+  print("if_date.people.b.age: ", if_date.people.b.age);
+}else if (if_date.animal.bird.power < 120) {
+  print("if_date.animal.bird.power: ",if_date.animal.bird.power);
+}else {
+  print("wrong!");
+}
+
+switch (if_date.people.a.age) {
+  case 18:
+    print("if_date.people.a.age = 18");
+    break;
+  case 19:
+    print("if_date.people.a.age = 19");
+  default:
+    print("if_date.people.a.age != 18, 19");
+}
 //-------------------------------------------
 // 7.3 循环
 //-------------------------------------------
+let i = 0;
+while (i < 100) {
+  print(i);
+  i++;
+}
 
+do {
+  print(i);
+  i++;
+} while (i < 200);
+
+for(; i < 300; i++) {
+  print(i);
+}
+
+for (var temp in if_date) {
+  print(temp);
+  for (var temp2 in if_date[temp]) {
+    print(temp2);
+  }
+}
 //-------------------------------------------
 // 7.4 跳出语句
 //-------------------------------------------
