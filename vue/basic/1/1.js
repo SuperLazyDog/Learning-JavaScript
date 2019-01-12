faae6c98ec69dbc9d4f945923beb56b1b3fca132
SuperLazyDog <jyoitatsu@foxmail.com>
2019-01-12 12:29:21 +0900

    Temp: [了解vue]

--- vue/basic/1/1.js
+++ vue/basic/1/1.js
@@ -1,4 +1,7 @@
 document.addEventListener("DOMContentLoaded", function () {
+  //------------------------------------------------------------
+  //                      宣言的レンダリング
+  //------------------------------------------------------------
   var app = new Vue({
     el: '#app',
     data: {
@@ -17,6 +20,10 @@ document.addEventListener("DOMContentLoaded", function () {
   app2.message = 'some new message';
 
 
+  //------------------------------------------------------------
+  //                      条件分岐とループ
+  //------------------------------------------------------------
+
   var app3 = new Vue({
     el: '#app-3',
     data: {
@@ -24,12 +31,76 @@ document.addEventListener("DOMContentLoaded", function () {
     }
   });
   // 五秒間、1秒ごとに点滅
-  var count = 0;
-  var id = setInterval(function () {
-    count += 1;
-    if (count >= 5) {
-      clearInterval(id);
+  // var count = 0;
+  // var id = setInterval(function () {
+  //   count += 1;
+  //   if (count >= 5) {
+  //     clearInterval(id);
+  //   }
+  //   app3.seen = !app3.seen;
+  // }, 1000);
+
+
+  //for
+  var app4 = new Vue({
+    el: "#app-4",
+    data: {
+      datas: [
+        { text: "first" },
+        { text: "second" },
+        { text: "third" }
+      ]
     }
-    app3.seen = !app3.seen;
-  }, 1000);
+  });
+
+  var app5 = new Vue({
+    el: '#app-5',
+    data: {
+      message: 'event listener'
+    },
+    methods: {
+      reverseMessage: function () {
+        this.message = this.message.split('').reverse().join('');
+      }
+    }
+  });
+
+  var app6 = new Vue({
+    el: '#app-6',
+    data: {
+      message: 'v-model sample'
+    }
+  });
+
+  Vue.component('cop-1', {
+    template: '<p>component sample 1</p>'
+  });
+
+  Vue.component('todo-item', {
+    template: '<li>This is a todo</li>'
+  });
+  var app7 = new Vue({
+    el: '#app-7'
+  });
+
+
+  Vue.component('cop-2', {
+    props: ["prop"],
+    template: "<p>prop: {{ prop }}</p>"
+  });
+
+  var app8 = new Vue({
+    el: '#app-8',
+    data: {
+      numbers: [
+        1, 2, 3, 4
+      ]
+    }
+  });
+
+  //------------------------------------------------------------
+  //                     ユーザー入力の制御
+  //------------------------------------------------------------
+  
+
 });
