6394fba4c9ea6267cf8bfd40f3fc5c0cdbe91a31
SuperLazyDog <jyoitatsu@gmail.com>
2019-04-12 14:10:19 +0900

    Done-step: [redo webpack get started]

--- node.js/webpack-demo/src/index.js
+++ node.js/webpack-demo/src/index.js
@@ -1,16 +1,11 @@
-import _ from 'lodash';
-import './style.css';
-import Icon from './icon.png';
-import Data from './sample.json'
+import _ from 'lodash'
 
 function component() {
-  let element = document.createElement('div');
-  element.innerHTML = _.join(['hello', 'webpack'], ' ');
-  element.classList.add('hello');
-  var myIcon = new Image();
-  myIcon.src = Icon;
-  element.appendChild(myIcon);
-  console.log(Data);
+  var element = document.createElement('div');
+
+  // Lodash（目前通过一个 script 脚本引入）对于执行这一行是必需的
+  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+
   return element;
 }
 
