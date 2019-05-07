a0c25429b70a38c0e6fe83a75b326bc1992b700c
SuperLazyDog <jyoitatsu@foxmail.com>
2019-05-07 17:02:23 +0900

    Done-step: [生命周期]

--- react/first_learning/src/index.js
+++ react/first_learning/src/index.js
@@ -5,10 +5,68 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 
 // ReactDOM.render(<App />, document.getElementById('root'));
+//-----------------------------------------------------------------------------
+// jsx
+//-----------------------------------------------------------------------------
+class JSX extends React.Component {
+  constructor() {
+    super();
+    this.buildName = (people) => {
+      return `${people.sex}的${people.name}`
+    };
+    this.people = {
+      name: '妖怪',
+      sex: '不男不女',
+    }
+    this.jsxElement = <p sampleProp={0}>{this.buildName(this.people)}</p>;
+    this.blankElement = <p/>;
+  }
+  render() {
+    return this.jsxElement;
+  }
+}
+// ReactDOM.render(
+//   <JSX test={12345}/>,
+//   document.getElementById('root')
+// );
+//-----------------------------------------------------------------------------
+// state&生命周期
+//-----------------------------------------------------------------------------
+class Clock extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      date: new Date()
+    };
+  }
+  componentDidMount() {
+    this.timerID = setInterval(
+      () => {
+        this.setState({
+          date: new Date()
+        });
+      },
+      1000
+    );
+  }
+  componentWillUnmount() {
+    clearInterval(this.timerID);
+  }
+  render() {
+    return (
+      <div>
+        <h1>Hello, world!</h1>
+        <h2>It is {this.state.date.toLocaleTimeString()}.</h2>
+      </div>
+    );
+  }
+}
+
 ReactDOM.render(
-  <p>sample</p>,
+  <Clock />,
   document.getElementById('root')
 );
+
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
