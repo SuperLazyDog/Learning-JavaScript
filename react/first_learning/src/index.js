a41b0452bab21c02219285e41f1bfbce8d4f642c
SuperLazyDog <jyoitatsu@foxmail.com>
2019-05-09 13:14:32 +0900

    Done-step: [状态提升]

--- react/first_learning/src/index.js
+++ react/first_learning/src/index.js
@@ -199,6 +199,100 @@ ReactDOM.render(
   (<Form><h1>inner</h1></Form>),
   document.getElementById('form')
 );
+//-----------------------------------------------------------------------------
+// 状态提升
+//-----------------------------------------------------------------------------
+function BoilingVerdict(props) {
+  if (props.celsius >= 100) {
+    return <p>The water would boil.</p>;
+  }
+  return <p>The water would not boil.</p>;
+}
+class Calculator extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {temperature: '', scale: ''};
+    this.handleCelsiusChnage = this.handleCelsiusChnage.bind(this);
+    this.handleFahrenheitChange = this.handleFahrenheitChange.bind(this);
+  }
+  handleCelsiusChnage(temperature) {
+    this.setState({scale: 'c', temperature})
+  }
+  handleFahrenheitChange(temperature) {
+    this.setState({scale: 'f', temperature});
+  }
+  render() {
+    const scale = this.state.scale;
+    const temperature = this.state.temperature;
+    const celsius = scale === 'f' ? tryConvert(temperature, toCelsius) : temperature;
+    const fahrenheit = scale === 'c' ? tryConvert(temperature, toFahrenheit) : temperature;
+    return (
+      <div>
+        <TemperatureInput
+          scale="c"
+          temperature={celsius}
+          onTemperatureChange={this.handleCelsiusChnage}
+        />
+        <TemperatureInput
+          scale="f"
+          temperature={fahrenheit}
+          onTemperatureChange={this.handleFahrenheitChange}
+        />
+        <BoilingVerdict
+          celsius={parseFloat(celsius)}
+        />
+      </div>
+    );
+  }
+}
+
+class TemperatureInput extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleChange = this.handleChange.bind(this);
+    this.scaleNames = {
+      c: 'Celsius',
+      f: 'Fahrenheit'
+    }
+  }
+  handleChange(e) {
+    this.props.onTemperatureChange(e.target.value);
+  }
+  render() {
+    const temperature = this.props.temperature;
+    const scale = this.props.scale;
+    return (
+      <fieldset>
+        <legend>Enter temperature in {this.scaleNames[scale]}</legend>
+        <input
+          type="text"
+          name="temperature"
+          value={temperature}
+          onChange={this.handleChange}
+        />
+      </fieldset>
+    );
+  }
+}
+function toCelsius(fahrenheit) {
+  return (fahrenheit-32)*5/9;
+}
+function toFahrenheit(celsius) {
+  return (celsius*9/5)+32;
+}
+function tryConvert(temperature, convert) {
+  const input = parseFloat(temperature);
+  if (Number.isNaN(input)) {
+    return '';
+  }
+  const output = convert(input);
+  const rounded = Math.round(output*1000)/1000;
+  return rounded.toString();
+}
+ReactDOM.render(
+  <Calculator />,
+  document.getElementById('stateup')
+);
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
