23577ef227ea0024b0efcb919c9a881fdf2f7ab0
SuperLazyDog <jyoitatsu@foxmail.com>
2019-06-07 18:29:49 +0900

    Done-step: [~表单输入绑定]

--- vue/first_learning/public/basic.js
+++ vue/first_learning/public/basic.js
@@ -52,8 +52,10 @@ for (var i = 0; i < 3; i++) {
   let temp = {};
   temp.id = i;
   temp.text = `测试数据${i+1}`;
+  temp.title = `uuid${i+1}`;
   list[i] = temp;
 }
+// Object.freeze(list);
 var app7 = new Vue({
   el: '#app-7',
   data: {
@@ -61,5 +63,118 @@ var app7 = new Vue({
   },
   methods: {
 
+  }
+});
+
+var app8 = new Vue({
+  el: '#app-8',
+  data: {
+    data: 100,
+    sample_class: 'sample',
+    sample_id: 'id'
+  },
+  methods: {
+    now: function() {
+      return new Date();
+    }
+  },
+  computed: {
+    computedData: {
+      get: function() {
+        console.log(this);
+        return this.data * 3;
+      },
+      set: function(newValue) {
+        this.data = newValue;
+      }
+    },
+    computed_now: function() {
+      return new Date();
+    }
+  },
+  watch: {
+    data: function() {
+      console.log('watching data now');
+    }
+  }
+});
+
+var app9 = new Vue({
+  el: "#app-9",
+  data: {
+    classes: {
+      isActive: true
+    }
+  },
+  props: [
+
+  ],
+  methods: {
+
+  }
+});
+
+var app10 = new Vue({
+  el: "#app-10",
+  data: {
+    show: true,
+    notShow: false
+  }
+});
+
+var app11 = new Vue({
+  el: "#app-11",
+  data: {
+    list,
+    sampleData: {
+      a: 'here is a',
+      b: 'here is b',
+      c: 'here is c'
+    }
+  },
+  computed: {
+    filteredList: function() {
+      return this.list.filter(function(v, i, a) {
+        return v.id%2 == 0;
+      });
+    }
+  }
+});
+
+var app12 = new Vue({
+  el: '#app-12',
+  data: {
+    click_count: 0
+  },
+  methods: {
+    msg: function(msg) {
+      console.log("fired");
+      this.click_count++;
+      // alert(msg);
+    },
+    debug: function(msg) {
+      console.log(`UUID ${msg}`);
+    }
+  }
+});
+
+var options = []
+for (var i = 0; i < 10; i++) {
+  options[i] = {};
+  options[i].text = i;
+  options[i].value = `UUID${i}`;
+}
+var app13 = new Vue({
+  el: '#app-13',
+  data: {
+    text_input: 'sample',
+    textarea_input: 'sample',
+    single_checkbox_input: true,
+    checkbox_input: [2, 3],
+    radio_input: null,
+    single_select_input: 'b',
+    select_input: [],
+    options,
+    vForSample: "UUID7"
   }
 });
