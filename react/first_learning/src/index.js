30c04476f4082402278ae57576c8719b6c65e91a
SuperLazyDog <jyoitatsu@foxmail.com>
2019-06-14 18:52:09 +0900

    Done-step: [完成import, export语法的实际学习]

--- react/first_learning/src/index.js
+++ react/first_learning/src/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+// import App from './App';
 import * as serviceWorker from './serviceWorker';
-
+import { debug } from './js_basic/import' // import, export test
 // ReactDOM.render(<App />, document.getElementById('root'));
 //-----------------------------------------------------------------------------
 // jsx
@@ -293,6 +293,11 @@ ReactDOM.render(
   <Calculator />,
   document.getElementById('stateup')
 );
+
+//-----------------------------------------------------------------------------
+// import, export
+//-----------------------------------------------------------------------------
+debug();
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
---
+++ react/first_learning/src/js_basic/export.js
@@ -0,0 +1,9 @@
+function sum(a, b) {
+  return a+b;
+}
+
+export { sum };
+export default sum;
+export function sum3(a, b, c) {
+  return a + b + c;
+}
